12dce090a536c52dc61f5f3356d3ada40ea3c2e9
vitrul <878574650@qq.com>
2022-04-26 19:22:15 +0800

    add gzip to compress files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+// gzip压缩
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_SRC : "/",
@@ -27,6 +29,17 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new ProgressBarPlugin(),  //打包进度条
+      new CompressionPlugin({
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        test: /\.js$|\.html$|\.css/,
+        threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+        minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+        // 删除原文件
+        // 如果开发环境要用就设置未false,不然编辑以后页面打不开
+        // 如果就生产环境用可以设置为true,这样打包后的体积更小
+        deleteOriginalAssets: false 
+      }),
     ],
     externals: process.env.NODE_ENV === 'production' ? {
       // 指定需要挂载的第三方库 格式：  第三方库名 ：'库在项目中的别名'
